a17aa33f6770c22899053a0f5e9a6544c48e60b1
1katoda <38652146+1katoda@users.noreply.github.com>
2019-05-28 11:47:44 +0200

    Update user_page.php
    Change <h2>Actions</h2> to <h2>{tr:actions}</h2> for translation

--- templates/user_page.php
+++ templates/user_page.php
@@ -125,7 +125,7 @@
   <h2>Python CLI Client</h2>
   <a href="clidownload.php" target="_blank">Download Python CLI Client</a>
 
-    <h2>Actions</h2>
+    <h2>{tr:actions}</h2>
 
         {tr:user_profile_delete_about_description_text}
     
